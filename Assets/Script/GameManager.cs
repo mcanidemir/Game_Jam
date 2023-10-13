408a913bfb9fc8ed38875cf3b3ff29cbec24456e
Bugraunluu <bugraunl0607@gmail.com>
2023-10-14 01:16:47 +0300

    a

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -7,6 +7,9 @@ public class GameManager : MonoBehaviour
     public GameObject verypast;
     public GameObject past;
     public GameObject future;
+    public GameObject verypastfloor;
+    public GameObject pastfloor;
+    public GameObject futurefloor;
 
     // Start is called before the first frame update
     void Start()
@@ -22,18 +25,27 @@ public class GameManager : MonoBehaviour
             verypast.SetActive(true);
             past.SetActive(false);
             future.SetActive(false);
+            verypastfloor.SetActive(true);
+            pastfloor.SetActive(false);
+            futurefloor.SetActive(false);
         }
         else if (Input.GetKeyDown(KeyCode.Alpha2))
         {
             past.SetActive(true);
             verypast.SetActive(false);
             future.SetActive(false);
+            verypastfloor.SetActive(false);
+            pastfloor.SetActive(true);
+            futurefloor.SetActive(false);
         }
         else if (Input.GetKeyDown(KeyCode.Alpha3))
         {
             past.SetActive(false);
             verypast.SetActive(false);
             future.SetActive(true);
+            verypastfloor.SetActive(false);
+            pastfloor.SetActive(false);
+            futurefloor.SetActive(true);
         }
     }
 
