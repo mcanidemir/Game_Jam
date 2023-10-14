7aec54701d0cb68a30e2a8f8fa921b5bb2db52a1
Bugraunluu <bugraunl0607@gmail.com>
2023-10-14 17:50:04 +0300

    a

---
+++ Assets/Script/MenuRotate.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuRotate : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Vector3 Rotation_Speed = new Vector3(0, 0, 45);
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Rotation_Speed);
+    }
+}
