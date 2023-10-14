0413021bcfe02ccbb5e66b99d607f665402f374d
Bugraunluu <bugraunl0607@gmail.com>
2023-10-15 00:05:27 +0300

    a

---
+++ Assets/Script/Look.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Look : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Transform m_transform;
+    void Start()
+    {
+        m_transform = this.transform;  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        LAMouse();
+    }
+    public void LAMouse()
+    {
+        Vector2 direction = Camera.main.ScreenToWorldPoint(Input.mousePosition) - m_transform.position;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        Quaternion rotation = Quaternion.AngleAxis(angle - 90, Vector3.forward);
+        m_transform.rotation = rotation;
+    }
+}
